33c1632da46d7725ebae3614f715d8003916ff6d
eleven26 <rubymay21s@gmail.com>
2022-08-24 10:26:43 +0800

    mod File

--- core/file.go
+++ core/file.go
@@ -8,12 +8,16 @@ import "time"
 type File interface {
 	// Key is the object key.
 	Key() string
+
 	// Type is the object type.
 	Type() string
+
 	// Size is the size of current file.
 	Size() int64
+
 	// ETag is etag return by cloud storage providers.
 	ETag() string
+
 	// LastModified is the time when the object was last updated.
 	LastModified() time.Time
 }
